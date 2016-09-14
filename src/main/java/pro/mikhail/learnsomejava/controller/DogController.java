9f1cd6faa3cccc10c3ee0b771b57f27d5059fdb4
Mikhail Prosuntsov <Mikhail_Prosuntsov@epam.com>
2016-09-14 11:46:01 +0300

    initializer removed

--- src/main/java/pro/mikhail/learnsomejava/controller/DogController.java
+++ src/main/java/pro/mikhail/learnsomejava/controller/DogController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.util.UriComponentsBuilder;
 import pro.mikhail.learnsomejava.model.Dog;
-import pro.mikhail.learnsomejava.model.InitialDogsInitializer;
 import pro.mikhail.learnsomejava.service.DogService;
 
 import java.util.List;
@@ -21,9 +20,6 @@ import java.util.List;
 
 @Controller
 public class DogController {
-
-
-    InitialDogsInitializer initializer = new InitialDogsInitializer();
 
     private DogService dogService;
 
@@ -46,7 +42,7 @@ public class DogController {
     @RequestMapping(method = RequestMethod.GET, value = "/dog", produces = "application/json")
     public ResponseEntity<List<Dog>> getAllDogs()  {
 
-        List<Dog> dogList = initializer.getDogList();
+        List<Dog> dogList = dogService.getAllDogs();
 
         if(dogList.size() > 0){
             return new ResponseEntity<>(dogList, HttpStatus.OK);
@@ -58,7 +54,7 @@ public class DogController {
     @RequestMapping(method = RequestMethod.GET, value = "/dog/{id}", produces = "application/json")
     public ResponseEntity<Dog> getOneDog(@PathVariable("id") int id)  {
 
-        Dog dog = initializer.getDogList().get(id);
+        Dog dog = dogService.getDog(id);
 
         if(dog != null){
             return new ResponseEntity<>(dog, HttpStatus.OK);
@@ -69,9 +65,12 @@ public class DogController {
     @RequestMapping(method = RequestMethod.POST, value = "/dog", produces = "text/html")
     public ResponseEntity<Dog> createDog(@RequestBody Dog dog, UriComponentsBuilder ucBuilder) {
 
-        initializer.getDogList().add(dog);
+        //return id when saving the dog
+        dogService.saveDog(dog);
         HttpHeaders headers = new HttpHeaders();
-        int dogIndex = initializer.getDogList().indexOf(dog);
+
+        //TODO: hernya kakaya-to
+        int dogIndex = dogService.getAllDogs().indexOf(dog);
 
         if(dogIndex != -1){
             headers.setLocation(ucBuilder.path("/dog/{id}").buildAndExpand(dogIndex).toUri());
@@ -83,22 +82,14 @@ public class DogController {
     @RequestMapping(method = RequestMethod.PUT, value = "/dog/{id}", produces = "application/json")
     public ResponseEntity<Dog> updateDog(@PathVariable("id") int id, @RequestBody Dog dog, UriComponentsBuilder ucBuilder) {
 
-        //not sure if it is needed, but let's keep for now
-        //probably better to check parameters in the request
-        Dog updatedDog = initializer.getDogList().get(id);
-        if (dog.getName() != null)
-            updatedDog.setName(dog.getName());
-        if (dog.getDateOfBirth() != null)
-            updatedDog.setDateOfBirth(dog.getDateOfBirth());
-        if (dog.getHeight() != 0)
-            updatedDog.setHeight(dog.getHeight());
-        if (dog.getWeight() != 0)
-            updatedDog.setWeight(dog.getWeight());
 
+        dogService.updateDog(id, dog);
 
-        initializer.getDogList().set(id, updatedDog);
         HttpHeaders headers = new HttpHeaders();
-        int dogIndex = initializer.getDogList().indexOf(updatedDog);
+       // int dogIndex = dogService.getDog(id);
+
+        //aaaaaaaaa
+        int dogIndex = 1;
 
         if(dogIndex != -1){
             headers.setLocation(ucBuilder.path("/dog/{id}").buildAndExpand(dogIndex).toUri());
@@ -110,7 +101,7 @@ public class DogController {
     @RequestMapping(method= RequestMethod.DELETE, value = "/dog/{id}", produces = "application/json")
     public ResponseEntity<Dog> removeDog(@PathVariable("id") int id) {
 
-        Dog removedDog = initializer.getDogList().remove(id);
+        Dog removedDog = dogService.removeDoge(id);
 
         if (removedDog != null){
             return new ResponseEntity<>(removedDog, HttpStatus.OK);
